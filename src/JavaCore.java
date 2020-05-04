c675f59bbc61b38a98bbf9d8fe3546d7d887d477
Anh Tuan <anhtuan@Anhs-MacBook-Pro.local>
2020-05-04 19:46:16 +0700

    rename file

--- src/JavaCore.java
+++ src/JavaCore.java
@@ -1,4 +1,4 @@
-public class javacore {
+public class JavaCore {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
--- src/JavaCore0305.java
+++ src/JavaCore0305.java
@@ -1,4 +1,4 @@
-public class javacore0305 {
+public class JavaCore0305 {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
     }
