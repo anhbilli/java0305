7fe2ed84722aa30613d671d589c2a0ebe9ce6e34
Anh Tuan <anhtuan@Anhs-MacBook-Pro.local>
2020-05-04 19:41:22 +0700

    first commit

---
+++ src/javacore.java
@@ -0,0 +1,5 @@
+public class javacore {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/javacore0305.java
@@ -0,0 +1,5 @@
+public class javacore0305 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
